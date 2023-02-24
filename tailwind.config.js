95930fc67a957b1fb42cf5e411c1f546143dffa4
daoodaba975 <davinhoba9@outlook.fr>
2023-02-24 22:22:35 +0000

    🎉 :: setup Tailwind in project.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 
+    // using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
