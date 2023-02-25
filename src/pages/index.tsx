fdbb5f94b330cf61c50b5e330f4c7734c1a9c1cf
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-02-25 14:50:22 +0000

    🆕 :: add the partner section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import PartnersSection from './components/PartnersSection'
 // import { Inter } from '@next/font/google'
 
 export default function Home() {
@@ -10,8 +11,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon/favicon.ico" />
       </Head>
-      <main className="">
-        <p className="text-9xl">Hello World</p>
+      <main className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <PartnersSection />
       </main>
     </>
   )
