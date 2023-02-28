fcc778c29df920359b9534b2e652ff9dfee398f0
daoodaba975 <davinhoba9@outlook.fr>
2023-02-28 12:36:27 +0000

    🔁 :: some update & add Discord svg.

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,16 +1,17 @@
-import { Space_Grotesk } from "@next/font/google";
+import GalsenDevLogo from "./GalsenDevLogo";
 
+import { Space_Grotesk } from "@next/font/google";
 const grotesk = Space_Grotesk({ subsets: ["latin"] });
 
 const Footer = () => (
   <footer
     aria-label="Site Footer"
-    className="max-w-screen-xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8 lg:pt-24"
+    className={`${grotesk.className} max-w-screen-xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8 lg:pt-24`}
   >
     <div className="pt-8 mt-16 border-t border-gray-100">
       <div className="flex items-center gap-4">
-        <GalseDevLogo />
-        <h2 className={`${grotesk.className} text-gray-900 text-3xl`}>
+        <GalsenDevLogo />
+        <h2 className="text-gray-900 text-3xl">
           Galsen Dev
         </h2>
       </div>
@@ -23,13 +24,16 @@ const Footer = () => (
         <ul className="flex justify-center gap-6 lg:justify-end">
           {/* TODO: Add the correct links */}
           <li>
-            <YoutubeLink />
+            <DiscordLink />
+          </li>
+          <li>
+            <TelegramLink />
           </li>
           <li>
             <TwitterLink />
           </li>
           <li>
-            <TelegramLink />
+            <YoutubeLink />
           </li>
           <li>
             <GithubLink />
@@ -40,53 +44,33 @@ const Footer = () => (
   </footer>
 );
 
-// TODO: This should be a component
-const GalseDevLogo = () => (
-  <svg className="w-9 h-9" viewBox="0 0 52 52" fill="none">
-    <g clip-path="url(#clip0_119_120)">
-      <circle
-        cx="26"
-        cy="26"
-        r="25"
-        fill="url(#paint0_linear_119_120)"
-        stroke="white"
-        stroke-width="2"
-      />
-      <path
-        d="M32.3991 10.1016C33.1716 10.4006 33.5555 11.2693 33.2564 12.0419L22.3394 40.2442C22.0404 41.0168 21.1716 41.4006 20.3991 41.1016C19.6265 40.8025 19.2427 39.9338 19.5417 39.1612L30.4587 10.9589C30.7578 10.1863 31.6265 9.80249 32.3991 10.1016Z"
-        fill="white"
-      />
-      <path
-        d="M6.70915 25.06L17.7092 16.56C19.4806 15.6743 21.2087 17.56 19.2092 19.0601C18.3965 19.6698 16.6424 21.0714 14.8152 22.5314C12.8268 24.1202 10.7517 25.7783 9.70945 26.56C11.7095 28.06 19.7092 34.5601 19.7092 34.5601C21.2087 36.06 19.4762 37.9735 17.7092 36.5601C16.6909 35.7457 15.1754 34.5996 13.5338 33.3582C11.1442 31.5511 8.48747 29.542 6.70915 28.0601C5.81973 27.318 5.70911 26.06 6.70915 25.06Z"
-        fill="white"
-      />
-      <path
-        d="M34.5714 16.56L45.5714 25.06C46.5714 26.06 46.4608 27.318 45.5714 28.0601C43.7931 29.542 41.1365 31.551 38.7469 33.3581C37.1052 34.5995 35.5896 35.7457 34.5714 36.5601C32.8044 37.9735 31.0718 36.06 32.5714 34.5601C32.5714 34.5601 40.5711 28.06 42.5711 26.56C41.5288 25.7783 39.4538 24.1203 37.4653 22.5314C35.6381 21.0714 33.884 19.6698 33.0714 19.0601C31.0718 17.56 32.7999 15.6743 34.5714 16.56Z"
-        fill="white"
-      />
-    </g>
-    <defs>
-      <linearGradient
-        id="paint0_linear_119_120"
-        x1="1.31288e-07"
-        y1="29"
-        x2="67"
-        y2="26"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop stop-color="#4986FF" />
-        <stop offset="1" stop-color="#0C48AE" />
-      </linearGradient>
-      <clipPath id="clip0_119_120">
-        <rect width="52" height="52" fill="white" />
-      </clipPath>
-    </defs>
-  </svg>
+const DiscordLink = () => (
+  <a
+    href="https://discord.gg/CKZcKqf"
+    rel="noreferrer"
+    target="_blank"
+    className="transition hover:opacity-75"
+  >
+    <span className="sr-only">Discord</span>
+
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -28.5 256 256"
+      className="w-6 h-6"
+      fill="#5865F2"
+      aria-hidden="true"
+    >
+      <g>
+          <path d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z" fill="#5865F2" fill-rule="nonzero">
+          </path>
+      </g>
+    </svg>
+  </a>
 );
 
 const TelegramLink = () => (
   <a
-    href="/"
+    href="https://t.me/galsendev221"
     rel="noreferrer"
     target="_blank"
     className="text-gray-700 transition hover:opacity-75"
@@ -94,7 +78,7 @@ const TelegramLink = () => (
     <span className="sr-only">Telegram</span>
     <svg
       className="w-6 h-6"
-      fill="currentColor"
+      fill="#0088CC"
       viewBox="0 0 256 256"
       aria-hidden="true"
     >
@@ -107,42 +91,18 @@ const TelegramLink = () => (
   </a>
 );
 
-const GithubLink = () => (
-  <a
-    href="/"
-    rel="noreferrer"
-    target="_blank"
-    className="text-gray-700 transition hover:opacity-75"
-  >
-    <span className="sr-only">GitHub</span>
-
-    <svg
-      className="w-6 h-6"
-      fill="currentColor"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-        clip-rule="evenodd"
-      />
-    </svg>
-  </a>
-);
-
 const TwitterLink = () => (
   <a
-    href="/"
+    href="https://twitter.com/galsendev221"
     rel="noreferrer"
     target="_blank"
-    className="text-gray-700 transition hover:opacity-75"
+    className="transition hover:opacity-75"
   >
     <span className="sr-only">Twitter</span>
 
     <svg
       className="w-6 h-6"
-      fill="currentColor"
+      fill="#1DA1F2"
       viewBox="0 0 24 24"
       aria-hidden="true"
     >
@@ -153,18 +113,18 @@ const TwitterLink = () => (
 
 const YoutubeLink = () => (
   <a
-    href="https://youtube.com"
+    href="https://www.youtube.com/@GalsenDev221"
     rel="noreferrer"
     target="_blank"
-    className="text-gray-700 transition hover:opacity-75"
+    className="transition hover:opacity-75"
   >
-    <span className="sr-only">Dribbble</span>
+    <span className="sr-only">YouTube</span>
 
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 256 180"
       className="w-6 h-6"
-      fill="currentColor"
+      fill="#FF0000"
       aria-hidden="true"
     >
       <path d="M250.346 28.075A32.18 32.18 0 0 0 227.69 5.418C207.824 0 127.87 0 127.87 0S47.912.164 28.046 5.582A32.18 32.18 0 0 0 5.39 28.24c-6.009 35.298-8.34 89.084.165 122.97a32.18 32.18 0 0 0 22.656 22.657c19.866 5.418 99.822 5.418 99.822 5.418s79.955 0 99.82-5.418a32.18 32.18 0 0 0 22.657-22.657c6.338-35.348 8.291-89.1-.164-123.134Z"></path>
@@ -172,6 +132,30 @@ const YoutubeLink = () => (
         fill="#FFF"
         d="m102.421 128.06l66.328-38.418l-66.328-38.418z"
       ></path>
+    </svg>
+  </a>
+);
+
+const GithubLink = () => (
+  <a
+    href="https://github.com/GalsenDev221"
+    rel="noreferrer"
+    target="_blank"
+    className="transition hover:opacity-75"
+  >
+    <span className="sr-only">GitHub</span>
+
+    <svg
+      className="w-6 h-6"
+      fill="#000000"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+        clip-rule="evenodd"
+      />
     </svg>
   </a>
 );
