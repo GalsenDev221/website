72035185b9d6769d5f67696569597b91fe4dae32
daoodaba975 <davinhoba9@outlook.fr>
2023-02-28 14:09:01 +0000

    ✅ :: errors components & pages done.

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import Error404 from "./components/errors/404Error"
+
+export default function Custom404() {
+  return (
+    <>
+      <Head>
+        <title>Oup !</title>
+        <meta name="description" content="La plus grande communauté de développeurs au Sénégal" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </Head>
+      <Error404/>
+    </>
+  );
+}
