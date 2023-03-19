4e20e424d4b962dac280bb2605f1de38e6cb0da8
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-03-19 16:24:22 +0000

    🆕 :: add transition to the navbar mobile menu

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
+import { useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import Link from "next/link";
 
 import Donation from "./announcements/Donation";
@@ -19,7 +19,6 @@ const Navbar = () => {
   ];
 
   return (
-    // TODO: transform the HTML attributes à la JSX
     <>
       <div className="fixed w-full z-20 sticky-nav">
         <Donation />
@@ -71,38 +70,57 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          {/* FIX: There is a slight vertical shift */}
-          <Dialog
-            as="div"
-            className="md:hidden"
-            open={isOpen}
-            onClose={setIsOpen}
+          <Transition
+            as={Fragment}
+            show={isOpen}
           >
-            <div className="fixed inset-0 z-20 bg-black opacity-50" />
-            <Dialog.Panel className="fixed inset-y-0 left-0 z-20 w-full overflow-y-auto bg-white px-4 py-4 max-w-[250px] sm:ring-1 sm:ring-gray-900/10">
-              <div className="flex items-center justify-between">
-                {/* <a href="#" className="-m-1.5 p-1.5 text-blue-600 font-bold"> */}
-                {/*   <GalsenDevLogo /> */}
-                {/* </a> */}
+            <Dialog as="div" className="md:hidden" onClose={setIsOpen}>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 z-20 bg-black bg-opacity-50" />
+              </Transition.Child>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 -translate-x-3"
+                enterTo="opacity-100 translate-x-0"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 -translate-x-3"
+              >
+                <Dialog.Panel className="fixed inset-y-0 left-0 z-20 w-full overflow-y-auto bg-white px-4 py-4 max-w-[250px] sm:ring-1 sm:ring-gray-900/10">
+                <div className="flex items-center justify-between">
+                  {/* TODO: Do we need the logo here? */}
 
-                <button
-                  type="button"
-                  className="-m-2.5 rounded-md p-2.5 text-gray-700 hover:text-gray-400"
-                  onClick={() => setIsOpen(false)}
-                >
-                  <span className="sr-only">Close menu</span>
-                  <svg className="w-8 h-8" viewBox="0 0 24 24">
-                    <path
-                      fill="currentColor"
-                      d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6Z"
-                    />
-                  </svg>
-                </button>
-              </div>
-              <div className="mt-12 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    {link.map((item, index) => (
+                  {/* <a href="#" className="-m-1.5 p-1.5 text-blue-600 font-bold"> */}
+                  {/*   <GalsenDevLogo /> */}
+                  {/* </a> */}
+
+                  <button
+                    type="button"
+                    className="-m-2.5 rounded-md p-2.5 text-gray-700 hover:text-gray-400"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <span className="sr-only">Close menu</span>
+                    <svg className="w-8 h-8" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6Z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <div className="mt-12 flow-root">
+                  <div className="-my-6 divide-y divide-gray-500/10">
+                    <div className="space-y-2 py-6">
+                      {link.map((item, index) => (
                         <Link
                           className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                           key={index}
@@ -110,15 +128,17 @@ const Navbar = () => {
                         >
                           {item.name}
                         </Link>
-                    ))}
-                  </div>
-                  <div className="py-6">
-                    <GithubLink />
+                      ))}
+                    </div>
+                    <div className="py-6">
+                      <GithubLink />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Dialog.Panel>
-          </Dialog>
+              </Dialog.Panel>
+              </Transition.Child>
+            </Dialog>
+          </Transition>
         </header>
       </div>
     </>
