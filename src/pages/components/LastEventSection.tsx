39e8e13611fcbe8e8a25d3477dc98889f69a131a
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-03-22 08:46:56 +0000

    🧹 :: solve the TODOS, not all though (closes #24)

--- src/pages/components/LastEventSection.tsx
+++ src/pages/components/LastEventSection.tsx
@@ -1,14 +1,16 @@
+import Image from "next/image";
+
 const LastEventSection = () => (
   <section>
     <div className="">
       <div className="grid grid-cols-1 lg:h-[600px] lg:grid-cols-2">
-        {/* TODO: Why did I add this z-10 */}
         <div className="relative z-10 lg:py-16">
           <div className="relative h-64 sm:h-80 lg:h-full">
-            {/* TODO: Use `next/image` here */}
-            <img
-              alt="Last event of Galsen Dev"
+            <Image
               src="/img/hacktoberfest-2022.png"
+              alt="Last event of Galsen Dev"
+              width={500}
+              height={500}
               className="absolute inset-0 h-full w-full object-cover"
             />
           </div>
@@ -20,8 +22,7 @@ const LastEventSection = () => (
           <div className="p-8 sm:p-16 lg:p-24">
             <h2 className="text-2xl font-bold sm:text-3xl">
               Notre dernier event: <br />
-              {/* TODO: Get the correct shade of blue for the accent color */}
-              <span className="text-blue-500">HacktoberFest 2022</span>
+              <span className="text-lighter">HacktoberFest 2022</span>
             </h2>
 
             <p className="mt-4 text-gray-500">
@@ -30,12 +31,11 @@ const LastEventSection = () => (
               Hacktoberfest 2022.
             </p>
 
-            {/* TODO: Choose the right color here */}
             <div className="mt-8 flex items-center gap-2 text-gray-500">
-              <svg viewBox="0 0 32 32" fill="currentColor" className="w-8 h-8">
+              <svg viewBox="0 0 32 32" fill="currentColor" className="w-6 h-6">
                 <path d="M16 2C13.0837 2.00344 10.2878 3.16347 8.22564 5.22563C6.16348 7.28778 5.00345 10.0837 5.00001 13C4.99652 15.3832 5.77499 17.7018 7.21601 19.6C7.21601 19.6 7.51601 19.995 7.56501 20.052L16 30L24.439 20.047C24.483 19.994 24.784 19.6 24.784 19.6L24.785 19.597C26.2253 17.6996 27.0034 15.3821 27 13C26.9966 10.0837 25.8365 7.28778 23.7744 5.22563C21.7122 3.16347 18.9163 2.00344 16 2ZM16 17C15.2089 17 14.4355 16.7654 13.7777 16.3259C13.1199 15.8864 12.6072 15.2616 12.3045 14.5307C12.0017 13.7998 11.9225 12.9956 12.0769 12.2196C12.2312 11.4437 12.6122 10.731 13.1716 10.1716C13.731 9.61216 14.4437 9.2312 15.2197 9.07686C15.9956 8.92252 16.7998 9.00173 17.5307 9.30448C18.2616 9.60723 18.8864 10.1199 19.3259 10.7777C19.7654 11.4355 20 12.2089 20 13C19.9987 14.0605 19.5768 15.0771 18.827 15.827C18.0771 16.5768 17.0605 16.9987 16 17Z" />
               </svg>
-              <span className="text-blue-600">Campus Xarala, Liberté 6</span>
+              <span className="text-gray-600">Campus Xarala, Liberté 6</span>
             </div>
           </div>
         </div>
--- src/pages/components/PartnersSection.tsx
+++ src/pages/components/PartnersSection.tsx
@@ -1,30 +1,48 @@
 const PartnersSection = () => (
   <section className="">
     <div className="flex flex-col items-center lg:flex-row lg:justify-between">
-      {/* TODO: ngir yallah use the right colors */}
-      <h2 className="text-blue-400 font-bold text-4xl sm:text-center lg:text-left">
+      <h2 className="text-lighter font-bold text-4xl sm:text-center lg:text-left">
         Ils nous ont fait <br />
-        <span className="text-blue-700">confiance</span>
+        <span className="text-darker">confiance</span>
       </h2>
       <div className="mt-8 flex flex-col gap-2 sm:flex-row lg:mt-0">
-        {/* TODO: these should be links */}
-        <div className="bg-gray-100 w-72 h-32 flex items-center justify-center">
-          <img
-            alt="Logo of Propel"
-            className="w-40"
-            src="https://propel.community/images/PropelLogoBlack.svg?6eed2deeadb712f68727cb157b49e956"
-          />
-        </div>
-        <div className="bg-gray-100 w-72 h-32 flex items-center justify-center">
-          <img
-            alt="Logo of Xarala"
-            className="w-40"
-            src="https://www.xarala.co/static/img/logo.b5700a8deb63.png"
-          />
-        </div>
+        <Partner
+          name="Propel"
+          link="https://propel.community/"
+          img="https://propel.community/images/PropelLogoBlack.svg?6eed2deeadb712f68727cb157b49e956"
+        />
+        <Partner
+          name="Xarala"
+          link="https://www.xarala.co/en/"
+          img="https://www.xarala.co/static/img/logo.b5700a8deb63.png"
+        />
       </div>
     </div>
   </section>
+);
+
+interface PartnerProps {
+  link: string;
+  img: string;
+  name: string;
+}
+
+const Partner = ({ link, img, name }: PartnerProps) => (
+  <a
+    href={link}
+    rel="noreferrer"
+    target="_blank"
+    className="bg-gray-100 w-72 h-32 flex items-center justify-center"
+  >
+    {/* TODO: maybe download these images or add the domains to the next config */}
+    <img
+      width={160}
+      height={160}
+      alt={`Logo of ${name}`}
+      src={img}
+      className="w-40"
+    />
+  </a>
 );
 
 export default PartnersSection;
