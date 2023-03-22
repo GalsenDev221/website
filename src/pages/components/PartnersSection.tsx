ba31d45f509dcdc12e644118516c06d276816818
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-03-22 06:54:10 +0000

    💄 :: use same container styles on the PartnerSection

--- src/pages/components/PartnersSection.tsx
+++ src/pages/components/PartnersSection.tsx
@@ -1,20 +1,31 @@
 const PartnersSection = () => (
-  <section className="mt-16 mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 lg:flex justify-between">
-    {/* TODO: ngir yallah use the right colors */}
-    <h2 className="text-blue-400 font-bold text-4xl">
-      Ils nous ont fait <br />
-      <span className="text-blue-700">confiance</span>
-    </h2>
-    <div className="mt-8 flex flex-col gap-2 sm:flex-row lg:mt-0">
-      {/* TODO: these should be links */}
-      <div className="bg-gray-100 w-80 h-32 flex items-center justify-center">
-        <img alt="Logo of Propel" className="w-40" src="https://propel.community/images/PropelLogoBlack.svg?6eed2deeadb712f68727cb157b49e956" />
-      </div>
-      <div className="bg-gray-100 w-80 h-32 flex items-center justify-center">
-        <img alt="Logo of Xarala" className="w-40" src="https://www.xarala.co/static/img/logo.b5700a8deb63.png" />
+  // <section className="mt-16 mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 lg:flex justify-between">
+  <section className="mx-auto max-w-screen-sm px-4 py-8 sm:py-12 sm:px-6 lg:max-w-screen-xl lg:py-16 lg:px-8">
+    <div className="flex flex-col items-center lg:flex-row lg:justify-between">
+      {/* TODO: ngir yallah use the right colors */}
+      <h2 className="text-blue-400 font-bold text-4xl sm:text-center lg:text-left">
+        Ils nous ont fait <br />
+        <span className="text-blue-700">confiance</span>
+      </h2>
+      <div className="mt-8 flex flex-col gap-2 sm:flex-row lg:mt-0">
+        {/* TODO: these should be links */}
+        <div className="bg-gray-100 w-72 h-32 flex items-center justify-center">
+          <img
+            alt="Logo of Propel"
+            className="w-40"
+            src="https://propel.community/images/PropelLogoBlack.svg?6eed2deeadb712f68727cb157b49e956"
+          />
+        </div>
+        <div className="bg-gray-100 w-72 h-32 flex items-center justify-center">
+          <img
+            alt="Logo of Xarala"
+            className="w-40"
+            src="https://www.xarala.co/static/img/logo.b5700a8deb63.png"
+          />
+        </div>
       </div>
     </div>
   </section>
-)
+);
 
-export default PartnersSection
+export default PartnersSection;
