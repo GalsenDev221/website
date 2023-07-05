e8afc3998e1093ea464fd4272205859ea5ddedc0
daoodaba975 <davinhoba9@outlook.fr>
2023-07-05 18:23:57 +0000

    ✔️ :: add new Head components & OG image.

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,18 +1,10 @@
-import Head from 'next/head';
+import Header from './components/Header';
 import Blank from './components/Blank';
 
 export default function About() {
 	return (
 		<>
-			<Head>
-				<title>Notre blog</title>
-				<meta
-					name="description"
-					content="Renforcer la scène tech sénégalaise, connecter les développeurs, partager la connaissance et offrir un soutien communautaire."
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon/favicon.ico" />
-			</Head>
+			<Header header="Notre blog" />
 			<Blank />
 		</>
 	);
