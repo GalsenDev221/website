1c8a5a89eefdc2d5b4c8559ddd2c1ea7aefe40a6
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2023-09-22 12:37:05 +0000

    🆕 :: basic design for the event page
    For now, the data is static and store in the EventContent component. I'll try to refactor it later.

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,13 +1,13 @@
-import Blank from '@/components/Blank';
-import Header from '@/components/Header';
 import useTranslation from 'next-translate/useTranslation';
+import Header from '@/components/Header';
+import EventContent from '@/components/events/EventContent';
 
 export default function Events() {
-	const { t, lang } = useTranslation('common');
+	const { t } = useTranslation('common');
 	return (
 		<>
 			<Header header={t('events.header')} />
-			<Blank />
+			<EventContent />
 		</>
 	);
 }
