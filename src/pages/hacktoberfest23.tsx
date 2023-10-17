6976c27053b44944bfc1f5ccd44e7af9df75904b
daoodaba975 <davinhoba9@outlook.fr>
2023-10-17 18:26:29 +0000

    🎉 :: new page for Hacktoberfest.

---
+++ src/pages/hacktoberfest23.tsx
@@ -0,0 +1,24 @@
+import Header from '@/components/Header';
+import Script from 'next/script';
+
+export default function About() {
+	return (
+		<>
+			<Header header="Hacktoberfest 2023 | Hackaton Session, Open-source Day & Talks" />
+			<iframe
+				data-tally-src="https://tally.so/embed/3xD4eG?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+				width="100%"
+				height="284"
+				title="Contact form"
+			></iframe>
+
+			<Script
+				id="tally-js"
+				src="https://tally.so/widgets/embed.js"
+				onLoad={() => {
+					Tally.loadEmbeds();
+				}}
+			/>
+		</>
+	);
+}
