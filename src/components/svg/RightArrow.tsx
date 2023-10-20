263518d73fa830f0b8118635041f98d8cea877b7
daoodaba975 <davinhoba9@outlook.fr>
2023-10-20 12:44:15 +0000

    ✅ :: fix SVG attribute error.

--- src/components/svg/RightArrow.tsx
+++ src/components/svg/RightArrow.tsx
@@ -8,9 +8,9 @@ const RightArrow = () => (
 			stroke="currentColor"
 		>
 			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
 				d="M17 8l4 4m0 0l-4 4m4-4H3"
 			/>
 		</svg>
