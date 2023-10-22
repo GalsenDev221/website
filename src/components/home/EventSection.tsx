591135b84fb5b7de8f9bc6d91f769d229676b49e
daoodaba975 <davinhoba9@outlook.fr>
2023-10-22 03:47:35 +0000

    🎉 :: add new coming Event. Hacktoberfest 2023

--- src/components/home/EventSection.tsx
+++ src/components/home/EventSection.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
 
-const LastEventSection = () => {
+const EventSection = () => {
 	const { t, lang } = useTranslation('home');
 	return (
 		<section>
@@ -10,8 +11,8 @@ const LastEventSection = () => {
 					<div className="relative z-10 lg:py-16">
 						<div className="relative h-64 sm:h-80 lg:h-full">
 							<Image
-								src="/img/hacktoberfest-2022.png"
-								alt="Last event of Galsen Dev"
+								src="https://res.cloudinary.com/degyjrpjj/image/upload/v1688584020/galsendev/main/bnr3qrdr4ptvwmwdbkop.png"
+								alt="Upcoming event of Galsen Dev"
 								width={500}
 								height={500}
 								className="absolute inset-0 h-full w-full object-cover"
@@ -40,7 +41,17 @@ const LastEventSection = () => {
 								>
 									<path d="M16 2C13.0837 2.00344 10.2878 3.16347 8.22564 5.22563C6.16348 7.28778 5.00345 10.0837 5.00001 13C4.99652 15.3832 5.77499 17.7018 7.21601 19.6C7.21601 19.6 7.51601 19.995 7.56501 20.052L16 30L24.439 20.047C24.483 19.994 24.784 19.6 24.784 19.6L24.785 19.597C26.2253 17.6996 27.0034 15.3821 27 13C26.9966 10.0837 25.8365 7.28778 23.7744 5.22563C21.7122 3.16347 18.9163 2.00344 16 2ZM16 17C15.2089 17 14.4355 16.7654 13.7777 16.3259C13.1199 15.8864 12.6072 15.2616 12.3045 14.5307C12.0017 13.7998 11.9225 12.9956 12.0769 12.2196C12.2312 11.4437 12.6122 10.731 13.1716 10.1716C13.731 9.61216 14.4437 9.2312 15.2197 9.07686C15.9956 8.92252 16.7998 9.00173 17.5307 9.30448C18.2616 9.60723 18.8864 10.1199 19.3259 10.7777C19.7654 11.4355 20 12.2089 20 13C19.9987 14.0605 19.5768 15.0771 18.827 15.827C18.0771 16.5768 17.0605 16.9987 16 17Z" />
 								</svg>
-								<span className="text-gray-600">Campus Xarala, Liberté 6</span>
+								<span className="text-gray-600">Campus Xarala, Sicap Mbao</span>
+							</div>
+
+							<div className="mt-4 md:mt-8">
+								<Link
+									href="https://discord.gg/CKZcKqf"
+									target="_blank"
+									className="inline-block rounded bg-baseColor px-12 py-3 text-sm font-medium text-white transition hover:bg-darker focus:outline-none focus:ring focus:ring-lighter"
+								>
+									{t('lastEvent.button')}
+								</Link>
 							</div>
 						</div>
 					</div>
@@ -50,4 +61,4 @@ const LastEventSection = () => {
 	);
 };
 
-export default LastEventSection;
+export default EventSection;
